8e90ccd46335e4d1c8e826de168a9ee5c60a8f25
Aakash Dhongade <dhongadeaakash@gmail.com>
2017-03-28 09:00:00 +0530

    Creating core files

--- server/controllers/diet.js
+++ server/controllers/diet.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 exports.getDiet=function(req,res){
 	//fetcht the data and display..
 
@@ -9,3 +11,24 @@ exports.getCalculateDiet=function(req,res)
 res.render('calculatediet')
 }
 
+exports.postCalculateDiet=function(req,res)
+{
+		var ethnicity=[]
+	// TODO
+
+	//take care of ethnicity one
+	// consider if user doesn`t enter all. 
+	
+		// console.log(req.body.ethnicity)
+		ethnicity=ethnicity.concat(req.body.ethnicity)
+		// console.log(ethnicity)
+		
+
+		// send the response here if execution time is long
+		
+		res.render('index');
+
+
+		console.log("exe after res.")
+
+	}
